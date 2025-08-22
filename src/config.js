3f0b5fce66d99e566a8de13342b40aeaa26f5742
camelCase <153457928+2000akansha@users.noreply.github.com>
2025-08-22 23:05:10 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,8 @@
 // export const BASE_URL ='https://innobles.com:1004/api/v1'
-export const BASE_URL = 'http://localhost:1030/api/v1'
+// export const BASE_URL = 'http://localhost:1030/api/v1'
+
+
+export const BASE_URL = "https://blogweb-backend-1it9.onrender.com/api/v1";
+
 
 export const INTEGRITY_SECRET_KEY = "saggitariusForever"
