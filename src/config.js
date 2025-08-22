3d439bc61a9c63917f985f62807ba2596be47529
Your Name <you@example.com>
2025-08-22 23:27:58 +0530

    versel.json added

--- src/config.js
+++ src/config.js
@@ -1,4 +1,10 @@
-// export const BASE_URL ='https://innobles.com:1004/api/v1'
-export const BASE_URL = 'http://localhost:1030/api/v1'
+// // export const BASE_URL ='https://innobles.com:1004/api/v1'
+// export const BASE_URL = 'http://localhost:1030/api/v1'
 
-export const INTEGRITY_SECRET_KEY = "saggitariusForever"
+// Local ke liye (jab local pe test karna ho)
+// export const BASE_URL = "http://localhost:1030/api/v1";
+
+// Live ke liye (jab frontend ko deploy karna ho)
+export const BASE_URL = "https://blogweb-backend-1it9.onrender.com/api/v1";
+
+export const INTEGRITY_SECRET_KEY = "saggitariusForever";
